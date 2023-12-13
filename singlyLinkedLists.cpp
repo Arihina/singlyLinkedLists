a5c0bda32ae016b9417218e8ba3bcb63827f41df
Alena Kozlova <alena.kozlova2004@mail.ru>
2023-12-13 22:30:24 +0300

    added function for to merge ordered lists

--- singlyLinkedLists.cpp
+++ singlyLinkedLists.cpp
@@ -8,10 +8,26 @@ void printList(SinglyLinkedList<T>&);
 
 SinglyLinkedList<int> fillIntOrderList(int, int, int);
 
+template<typename T>
+SinglyLinkedList<T> mergeLists(SinglyLinkedList<T>&, SinglyLinkedList<T>&);
+
 int main()
 {
-    SinglyLinkedList<int> list = fillIntOrderList(1, 6, 1);
-    printList(list);
+    // demonstration of merging two ordered lists
+    SinglyLinkedList<int> list1 = fillIntOrderList(0, 16, 3);
+    SinglyLinkedList<int> list2 = fillIntOrderList(3, 7, 1);
+
+    cout << "List 1" << endl;
+    printList(list1);
+    cout << "List 2" << endl;
+    printList(list2);
+
+	SinglyLinkedList<int> list3 = mergeLists(list1, list2);
+
+    cout << "List 3 (merging list 1 with list 2)" << endl;
+	printList(list3);
+
+    
 
 }
 
@@ -25,6 +41,7 @@ void printList(SinglyLinkedList<T>& list) {
     cout << endl;
 }
 
+
 SinglyLinkedList<int> fillIntOrderList(int leftLim, int rightLim, int step) {
 
     SinglyLinkedList<int> list;
@@ -33,4 +50,42 @@ SinglyLinkedList<int> fillIntOrderList(int leftLim, int rightLim, int step) {
         list.appendElem(i);
     }
     return list;
+}
+
+
+template<typename T>
+SinglyLinkedList<T> mergeLists(SinglyLinkedList<T>& list1, SinglyLinkedList<T>& list2) {
+    SinglyLinkedList<T> newList;
+
+    int len1 = list1.getLength(), len2 = list2.getLength();
+    int i = 0, j = 0;
+
+    while (i < len1 && j < len2)
+    {
+        if (list1[i] <= list2[j])
+        {
+            newList.appendElem(list1[i++]);
+        }
+        else
+        {
+            newList.appendElem(list2[j++]);
+        }
+    }
+
+    if (i < len1)
+    {
+        for (int k = i; k < len1; k++)
+        {
+            newList.appendElem(list1[k]);
+        }
+    }
+    else if (j < len2)
+    {
+        for (int k = j; k < len2; k++)
+        {
+            newList.appendElem(list2[k]);
+        }
+    }
+
+	return newList;
 }
